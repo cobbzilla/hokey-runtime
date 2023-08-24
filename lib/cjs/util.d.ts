a70aafcc7edd1a8b7404e20a178a070609d2d1fb
Jonathan Cobb <jonathan@kyuss.org>
2023-08-24 02:19:48 -0400

    add ISO_639 map

--- lib/cjs/util.d.ts
+++ lib/cjs/util.d.ts
@@ -1,5 +1,3 @@
-export declare const ALL_LANGS = "af,sq,am,ar,hy,az,eu,be,bn,bs,bg,ca,ceb,zh,co,hr,cs,da,nl,en,eo,et,fi,fr,fy,gl,ka,de,el,gu,ht,ha,haw,he,iw,hi,hmn,hu,is,ig,id,ga,it,ja,jv,kn,kk,km,rw,ko,ku,ky,lo,la,lv,lt,lb,mk,mg,ms,ml,mt,mi,mr,mn,my,ne,no,ny,or,ps,fa,pl,pt,pa,ro,ru,sm,gd,sr,st,sn,sd,si,sk,sl,so,es,su,sw,sv,tl,tg,ta,tt,te,th,tr,tk,uk,ur,ug,uz,vi,cy,xh,yi,yo,zu";
-export declare const ALL_LANGS_ARRAY: string[];
 export declare const unknownMessage: (msg: string) => string;
 export declare const isUnknownMessage: (msg: string) => boolean;
 export declare const messageExists: (msgKey: string, messages: HokeyLocaleMessages) => boolean;
--- lib/esm/index.d.ts
+++ lib/esm/index.d.ts
@@ -1,3 +1,4 @@
+export * from "./constants.js";
 export * from "./util.js";
 export * from "./parse.js";
 export * from "./message.js";
